c4c12ceb9a6f24653f5a2807cc27dba8ed8d6dd3
Guilherme Gouveia Figueiredo Lima <ggfl@cin.ufpe.br>
2018-11-17 15:41:28 -0300

    Scene intersect method

--- Scene/Scene.cpp
+++ Scene/Scene.cpp
@@ -3,3 +3,26 @@
 //
 
 #include "Scene.h"
+
+
+Scene::Scene() = default;
+
+
+void Scene::add(Object* object){
+    this->objects.push_back(object);
+}
+
+Vec3 Scene::trace(const Ray &r, int recursionLevel) const {
+    //Phong
+}
+
+bool Scene::intersect(const Ray &r, ObjectIntersectionInfo *info) const {
+    for(auto obj : this->objects){
+        if(obj->intersect(r, info)){
+            return true;
+        }
+    }
+    return false;
+}
+
+
--- Scene/Scene.h
+++ Scene/Scene.h
@@ -5,8 +5,20 @@
 #ifndef PGAGORAVAI_SCENE_H
 #define PGAGORAVAI_SCENE_H
 
+#include <vector>
+#include "../Object/Object.h"
 
 class Scene {
+    std::vector<Object*> objects;
+public:
+
+    Scene();
+
+    bool intersect(const Ray& r, ObjectIntersectionInfo* info= nullptr) const;
+
+    Vec3 trace(const Ray& r,int recursionLevel = 0) const ;
+
+    void add(Object* object);
 };
 
 
