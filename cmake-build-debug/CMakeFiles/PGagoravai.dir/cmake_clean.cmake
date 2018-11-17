bdbd605e6ce38885f481be5185b078e92c00a802
pedro <phls73376@gmail.com>
2018-11-17 09:27:17 -0300

    agora vai começar

---
+++ cmake-build-debug/CMakeFiles/PGagoravai.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PGagoravai.dir/main.cpp.o"
+  "PGagoravai.pdb"
+  "PGagoravai"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/PGagoravai.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
